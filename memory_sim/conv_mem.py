5af7fc07ee30987ca7fd52ce65a62631f270fac8
enhoshen <enhoshen@gmail.com>
2017-05-01 09:27:05 -0400

    Do_row() for layer

--- memory_sim/conv_mem.py
+++ memory_sim/conv_mem.py
@@ -6,63 +6,83 @@ Created on Tue Jan 17 06:54:31 2017
 @author: enhoshen
 """
 import numpy as np
-import * from parameter
+from parameter import *
+
 
 def Unit(num):
 	i=0
 	_str=''
-	_num=float(num)
+	_num=num
 	_unit=[0,0,0,0,0]
-	while( _num>1024.0):
-		_unit[4-i]=_num % 1024.0
-		_num = _num/ 1024.0
+	while( _num>1024):
+		_unit[4-i]=_num % 1024
+		_num = _num / 1024
 		i += 1
 	_unit[4-i]=_num
-	for i , u in enumerate _unit:
+	for i , u in enumerate(_unit):
 		if u != 0:
-			_str+= u+{0:'T',1:'G',2:'M',3:'K',4:''}[i]  
+			_str+= str(u)+{0:'T',1:'G',2:'M',3:'K',4:''}[i] 
 	return _str
 
 class Layer:
 
 
-	def __init__(self,com_arg=layer_com_arg_def,in_arg=in_arg_def,flt_arg=flt_arg_def):
-		self.type       = com_arg['type']
-		self.stride      = com_arg['stride']
-		self.batch      = com_arg['batch']
-		self.channels   = com_arg['channels']
-		self.filters    = com_arg['filters']
-		self.in_prec    = com_arg['in_prec']
-		self.tp_prec    = com_arg['tp_prec']
-		self.o_prec     = com_arg['o_prec']
-		self.fps_goal   = com_arg['fps_goal']
+	def __init__(self,in_shape,out_shape,com_arg=None,in_arg=None,flt_arg=None
+	
+	            ):
+				
+		self.type       = layer_com_arg_def['type']
+		self.stride     = layer_com_arg_def['stride']
+		self.tp_prec    = layer_com_arg_def['tp_prec']
+		self.fps_goal   = layer_com_arg_def['fps_goal']
 		
-		self.in_h  = in_arg['in_height']
-		self.in_w  = in_arg['in_width']
-		self.in_ch = in_arg['in_channel']
+		self.in_h    = in_arg_def['in_height']
+		self.in_w    = in_arg_def['in_width']
+		self.in_ch   = in_arg_def['in_channel']
+		self.in_prec = in_arg_def['in_prec']
 		
-		self.flt_h = flt_arg['flt_height']
-		self.flt_w = flt_arg['flt_widht']
-		self.out_ch= flt_arg['out_channel']
-
+		self.flt_h    = flt_arg_def['flt_height']
+		self.flt_w    = flt_arg_def['flt_width']
+		self.out_ch   = flt_arg_def['out_channel']
+		self.out_prec = flt_arg_def['out_prec']
+		if in_arg != None and flt_arg !=None:
+			self.in_h    = in_shape[0]
+			self.in_w    = in_shape[1]
+			self.in_ch   = in_shape[2]
+			self.in_prec = in_shape[3]
+			
+			self.flt_h    = out_shape[0]
+			self.flt_w    = out_shape[1]
+			self.out_ch   = out_shape[2]
+			self.out_prec = out_shape[3]
 	def XB_count(self):
-		o_pixel=self.flt_h*self.flt_w*self.in_ch
-		o_frame=o_pixel*self.in_h*self.in_w/(self.stride*self.stride)
-		o_volume=o_frame*self.self.out_ch
-		precision=self.in_prec*self.
-		return o_volume*precision*self.batch
+		o_pixel  =self.flt_h*self.flt_w*self.in_ch
+		o_frame  =o_pixel*self.in_h*self.in_w/(self.stride*self.stride)
+		o_volume =o_frame*self.out_ch
+		precision=self.in_prec*self.out_prec
+		return o_volume*precision
 	def Size(self):
-		_weight=self.filter_shape[0]*self.filter_shape[1]*self.filter_shape[2]*self.filter_shape[3]*self.filter_shape[4]/8
-		_input= self.input_shape[0]*self.input_shape[1]*self.input_shape[2]*self.input_shape[3]*self.input_shape[4]/8
-		print(Unit(_weight)+'B for weights; '+Unit(_input)+'B for activations')
-		return [_weight , _input]
-	def Do_row (self, row_cnt , flt_cnt=1 ,ch_cnt=8): #parallel do *_cnt , return number of repetition
-		_row_rep=self.input_shape[1]/self.stride/row_cnt
-		_ch_rep=self.input_shape[3]/ch_cnt
-		_flt_rep=self.filter_shape[3]/flt_cnt
-		return _row_rep*_ch_rep*_flt_rep
-	def __repr__(self):
+		weight_size=self.flt_h*self.flt_w*self.in_ch*self.out_ch*self.in_prec/8
+		input_size =self.in_h*self.in_w*self.in_ch*self.out_prec/8
+		print(Unit(weight_size)+' Byte for weights; '+Unit(input_size)+' Byte for activations')
+		return weight_size, input_size
+	def Do_row (self, batch ,row_cnt , flt_cnt=1 ,ch_cnt=8): #parallel do *_cnt , return number of repetition
+		row_rep=self.in_h/self.stride/row_cnt
+		ch_rep =self.in_ch/ch_cnt
+		flt_rep=self.out_ch/flt_cnt
+		weight_size=ch_cnt*flt_cnt*self.flt_w*self.flt_h/8  
+		input_size =self.in_w*row_cnt*ch_cnt/8
+		rep_cnt=row_rep*ch_rep*flt_rep*self.out_prec*self.in_prec
+		
+		return rep_cnt , {'unique_weight_size':weight_size,   
+		                  'eyeriss_weight_size':weight_size*batch*row_cnt,
+						  'unique_input_size' :input_size,
+						  'eyeriss_input_size':input_size*self.flt_h
+                         }
+	def Do_pix (self):
 		return 0
+	def __repr__(self):
+		return ''
 	def __str__(self):
 		return ''
 
@@ -70,6 +90,8 @@ class Model:
 	def __init__(self, layers):
 		self.layers=layers
 		self.layer_size=len(layers)
+	def append(self,layer):
+		self.layers.append(layer)
 	def XBC(self):
 		_xbc = 0
 		for layer in self.layers:
@@ -81,8 +103,8 @@ class Architecture:
 	def __init__(self,model, conf=[14,10,'1D',5,4]):
 		self.name='generated'
 		self.model=model
-    def peak_performance(self):
-		
+	def peak_performance(self):
+		return 
 	def buffer_requirment(self,i):
 		_w,_x=model.layers[i].Size()
 		_conf =model.layers[i].conf
@@ -93,17 +115,20 @@ class Architecture:
 	def compute_layer (self,i):
 		#TODO
 		return [off_chip,on_chip]
-	def FPS_xbc(self)
-	
+	def FPS_xbc(self):
+		return
 	def __repr__(self):
 		return 0
 	def __str__(self):
 		return ''
-test=Layer([1,7,7,64,1],[3,3,64,128,1])
-test2=Layer([1,7,7,128,1],[3,3,64,64,1])
+test=Layer([7,7,64,1],[3,3,128,1])
+test2=Layer([7,7,128,1],[3,3,64,1])
 
 Wrap=[test,test2]
 test.Size()
+rep , _ = test.Do_row(4 , 7, 1 , 16)
+print str(rep) + 'repetition'
+print _
 m1=Model(Wrap)
 print(Unit(test.XB_count()))
 print(m1.XBC())
--- memory_sim/parameter.py
+++ memory_sim/parameter.py
@@ -3,21 +3,21 @@ layer_com_arg_def={ 'type':'conv'  ,  # common arguments
 				'batch':4     ,
 				'channels':64 ,
 				'filters':32  ,
-				'in_prec':2   ,
 				'tp_prec':12  ,
-				'o_prec' :2   ,
 				'fps_goal':30
 				
 		}
 in_arg_def={    'batch':4,
 				'in_height':7,
 				'in_width' :7,
-				'in_channel':32
+				'in_channel':32,
+				'in_prec':2   
 		}
 flt_arg_def={       'flt_height':3,
 					'flt_width' :3,
 					'in_channel':32,
-					'out_channel':32
+					'out_channel':32,
+					'out_prec' :2   
 		}
 arch_com_arg_def={ 'PE_width':14    ,
 				'PE_height':12   ,
