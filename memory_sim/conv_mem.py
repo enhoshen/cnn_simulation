64ca8d6ee657d86ed766120bb838da7158c25395
enhoshen <enhoshen@gmail.com>
2017-05-01 11:43:07 -0400

    save progress

--- memory_sim/conv_mem.py
+++ memory_sim/conv_mem.py
@@ -27,34 +27,27 @@ def Unit(num):
 class Layer:
 
 
-	def __init__(self,in_shape,out_shape,com_arg=None,in_arg=None,flt_arg=None
-	
-	            ):
-				
-		self.type       = layer_com_arg_def['type']
-		self.stride     = layer_com_arg_def['stride']
-		self.tp_prec    = layer_com_arg_def['tp_prec']
-		self.fps_goal   = layer_com_arg_def['fps_goal']
-		
-		self.in_h    = in_arg_def['in_height']
-		self.in_w    = in_arg_def['in_width']
-		self.in_ch   = in_arg_def['in_channel']
-		self.in_prec = in_arg_def['in_prec']
-		
-		self.flt_h    = flt_arg_def['flt_height']
-		self.flt_w    = flt_arg_def['flt_width']
-		self.out_ch   = flt_arg_def['out_channel']
-		self.out_prec = flt_arg_def['out_prec']
-		if in_arg != None and flt_arg !=None:
-			self.in_h    = in_shape[0]
-			self.in_w    = in_shape[1]
-			self.in_ch   = in_shape[2]
-			self.in_prec = in_shape[3]
+	def __init__(self,in_shape,out_shape,com_arg=None):
+		if com_arg != None:		
+			self.type       = com_arg['type']
+			self.stride     = com_arg['stride']
+			self.tp_prec    = com_arg['tp_prec']
+			self.fps_goal   = com_arg['fps_goal']
+		else:
+			self.type       = layer_com_arg_def['type']
+			self.stride     = layer_com_arg_def['stride']
+			self.tp_prec    = layer_com_arg_def['tp_prec']
+			self.fps_goal   = layer_com_arg_def['fps_goal']
 			
-			self.flt_h    = out_shape[0]
-			self.flt_w    = out_shape[1]
-			self.out_ch   = out_shape[2]
-			self.out_prec = out_shape[3]
+		self.in_h    = in_shape[0]
+		self.in_w    = in_shape[1]
+		self.in_ch   = in_shape[2]
+		self.in_prec = in_shape[3]
+		
+		self.flt_h    = out_shape[0]
+		self.flt_w    = out_shape[1]
+		self.out_ch   = out_shape[2]
+		self.out_prec = out_shape[3]
 	def XB_count(self):
 		o_pixel  =self.flt_h*self.flt_w*self.in_ch
 		o_frame  =o_pixel*self.in_h*self.in_w/(self.stride*self.stride)
@@ -62,10 +55,9 @@ class Layer:
 		precision=self.in_prec*self.out_prec
 		return o_volume*precision
 	def Size(self):
-		weight_size=self.flt_h*self.flt_w*self.in_ch*self.out_ch*self.in_prec/8
-		input_size =self.in_h*self.in_w*self.in_ch*self.out_prec/8
-		print(Unit(weight_size)+' Byte for weights; '+Unit(input_size)+' Byte for activations')
-		return weight_size, input_size
+		weight_size=self.flt_h*self.flt_w*self.in_ch*self.out_ch*self.out_prec/8
+		input_size =self.in_h*self.in_w*self.in_ch*self.in_prec/8
+		return weight_size, input_size ,Unit(weight_size)+' Byte for weights; '+Unit(input_size)+' Byte for activations'
 	def Do_row (self, batch ,row_cnt , flt_cnt=1 ,ch_cnt=8): #parallel do *_cnt , return number of repetition
 		row_rep=self.in_h/self.stride/row_cnt
 		ch_rep =self.in_ch/ch_cnt
@@ -93,16 +85,24 @@ class Model:
 	def append(self,layer):
 		self.layers.append(layer)
 	def XBC(self):
-		_xbc = 0
-		for layer in self.layers:
-			_xbc += layer.XB_count()
-		#xnor bitcount counts
-		return Unit(_xbc)
+		XBC= [layer.XB_count() for layer in self.layers]
+		return XBC
+	def Size(self):
+		w_size = [layer.Size()[0]for layer in self.layers]	
+		in_size =  [layer.Size()[1]for layer in self.layers]	
+		return w_size,in_size ,Unit(np.sum(w_size))+' Byte for weights; '+Unit(np.sum(in_size))+' Byte for activations'
+		
+
 class Architecture:
 
-	def __init__(self,model, conf=[14,10,'1D',5,4]):
-		self.name='generated'
+	def __init__(self, model ,shape , com_arg = None):
+		
+		self.pe_w= shape[0]
+		self.pe_h= shape[1]
+		self.gbuf_size=shape[2]
+		self.bc_w= shape[3]
 		self.model=model
+		
 	def peak_performance(self):
 		return 
 	def buffer_requirment(self,i):
@@ -121,14 +121,18 @@ class Architecture:
 		return 0
 	def __str__(self):
 		return ''
+def alex_model():
+	l1=Layer([224,224,3,8],[11,11,64,1],{'type':'conv','stride':4,'tp_prec':12,'fps_goal':30})
+	l2=Layer([55,55,64,1],[5,5,128,1],{'type':'conv','stride':2,'tp_prec':12,'fps_goal':30})
+	return [l1,l2]
 test=Layer([7,7,64,1],[3,3,128,1])
 test2=Layer([7,7,128,1],[3,3,64,1])
 
-Wrap=[test,test2]
-test.Size()
+Wrap=[test,test2,test2]
+print test.Size()[2]
 rep , _ = test.Do_row(4 , 7, 1 , 16)
 print str(rep) + 'repetition'
 print _
 m1=Model(Wrap)
-print(Unit(test.XB_count()))
-print(m1.XBC())
+print m1.Size()
+
